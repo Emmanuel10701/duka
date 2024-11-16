acc1b0e9bc23314f45c9d91234a2f8e6238d3191
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-16 19:26:15 +0300

    removed the suer models in the models.py

--- my_app/models.py
+++ my_app/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=225)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- my_project/settings.py
+++ my_project/settings.py
@@ -131,12 +131,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = "/static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# To allow serving static files in development
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
